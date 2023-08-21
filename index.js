83b0e2116ff9d36842185414d926ae3187de7884
Ramazan <airtribunal@gmail.com>
2023-08-21 18:05:41 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = "6050687848:AAFl1zUnFaMhJ6K9ONMA60dxo-ouQjFG0xA";
+const webAppUrl = "https://dcf9-185-12-224-84.ngrok-free.app/";
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if (text === "/start") {
+        await bot.sendMessage(
+            chatId,
+            "Нажмите, чтобы открыть приложение",
+            {
+                reply_markup: {
+                    inline_keyboard: [
+                        [{ text: "Перейти", web_app: { url: webAppUrl } }],
+                    ],
+                },
+            }
+        );
+    }
+});
