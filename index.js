3bc0f70595547ce323bebbfc479a966f31c260ba
Ramazan <airtribunal@gmail.com>
2023-08-29 14:45:17 +0300

    feat/prod-bot-token

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-const token = "6665798120:AAGdU4EoKozc_J-pIA4w-6SskJo-HJ-x0nA";
+const token = "6050687848:AAFl1zUnFaMhJ6K9ONMA60dxo-ouQjFG0xA";
 // const webAppUrl = "https://cdd1-185-12-224-117.ngrok-free.app/?name=ramazan&age=17";
 const webAppUrl = 'https://dashboard-frontend-tg.vercel.app/?name=Ramazan&age=17'
 const bot = new TelegramBot(token, { polling: true });
