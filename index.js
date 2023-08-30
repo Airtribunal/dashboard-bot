8a5375101bb0c809ef6ecce97e4ba55003c0519b
Ramazan <airtribunal@gmail.com>
2023-08-30 12:30:22 +0300

    feat/production-bot

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const TelegramBot = require("node-telegram-bot-api");
-require('dotenv').config()
+require("dotenv").config();
 
-const token = process.env.BOT_TOKEN_DEV;
-const webAppUrl = process.env.WEB_APP_URL_PROD
+const token = process.env.BOT_TOKEN_PROD;
+const webAppUrl = process.env.WEB_APP_URL_PROD;
 const bot = new TelegramBot(token, { polling: true });
-
 
 bot.on("message", async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
 
     if (text === "/start") {
-        await bot.sendMessage(
-            chatId,
-            "Нажмите, чтобы открыть приложение",
-            {
-                reply_markup: {
-                    inline_keyboard: [
-                        [{ text: "Перейти", web_app: { url: webAppUrl } }],
-                    ],
-                },
-            }
-        );
+        await bot.sendMessage(chatId, "Нажмите, чтобы открыть приложение", {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: "Перейти", web_app: { url: webAppUrl } }],
+                ],
+            },
+        });
     }
 });
