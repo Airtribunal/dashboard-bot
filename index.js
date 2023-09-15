2c94e9b0d88753eba036ca517443c98ff35b8970
Ramazan <airtribunal@gmail.com>
2023-09-15 11:37:06 +0400

    feat/encode-uri

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const TelegramBot = require("node-telegram-bot-api");
 require("dotenv").config();
 
+const webAppUrl = encodeURI(process.env.WEB_APP_URL_PROD);
 const token = process.env.BOT_TOKEN_PROD;
-const webAppUrl = process.env.WEB_APP_URL_PROD;
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on("message", async (msg) => {
@@ -10,7 +10,7 @@ bot.on("message", async (msg) => {
     const text = msg.text;
 
     if (text === "/start") {
-        await bot.sendMessage(chatId, "Нажмите, чтобы открыть приложение", {
+        await bot.sendMessage(chatId, "Нажмите, чтобы открыть приложение Дашборда.", {
             reply_markup: {
                 inline_keyboard: [
                     [{ text: "Перейти", web_app: { url: webAppUrl } }],
