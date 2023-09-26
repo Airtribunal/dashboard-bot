28aea66752341f6f84cfbf4111f41d92ed8954d1
Ramazan <airtribunal@gmail.com>
2023-09-26 10:53:19 +0400

    feat

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const TelegramBot = require("node-telegram-bot-api");
 require("dotenv").config();
 
-const webAppUrl = encodeURI(process.env.WEB_APP_URL_DEV);
-const token = process.env.BOT_TOKEN_DEV;
+const webAppUrl = encodeURI(process.env.WEB_APP_URL_POD);
+const token = process.env.BOT_TOKEN_PROD;
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on("message", async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
-   
+    if (text !== "/start") {
+        await bot.sendMessage(chatId, 'Выполните команду /start для обновления ссылки Дашборда.')
+    }
     if (text === "/start") {
         await bot.sendMessage(chatId, "Нажмите, чтобы открыть приложение Дашборда.\nВыполните команду /start для новой кнопки", {
             reply_markup: {
